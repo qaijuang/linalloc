eb9739dd92b2c70fab495d1750fcb1ecbcc7ad17
qaijuang <qaijuang@gmail.com>
2026-06-03 15:04:11 -0400

    constrain lazy feature against target gate

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,21 @@
 #![warn(clippy::pedantic)]
 #![doc = include_str!("../README.md")]
 
+#[cfg(all(feature = "lazy", not(any(unix, windows))))]
+compile_error!("the `lazy` feature is currently supported only on Unix and Windows targets");
+
 mod bump_arena;
-#[cfg(feature = "lazy")]
+#[cfg(all(feature = "lazy", any(unix, windows)))]
 mod bump_arena_lazy;
-#[cfg(feature = "lazy")]
+#[cfg(all(feature = "lazy", any(unix, windows)))]
 pub(crate) mod sys;
 mod typed_arena;
-#[cfg(feature = "lazy")]
+#[cfg(all(feature = "lazy", any(unix, windows)))]
 mod typed_arena_lazy;
 
 pub use bump_arena::*;
-#[cfg(feature = "lazy")]
+#[cfg(all(feature = "lazy", any(unix, windows)))]
 pub use bump_arena_lazy::*;
 pub use typed_arena::*;
-#[cfg(feature = "lazy")]
+#[cfg(all(feature = "lazy", any(unix, windows)))]
 pub use typed_arena_lazy::*;
